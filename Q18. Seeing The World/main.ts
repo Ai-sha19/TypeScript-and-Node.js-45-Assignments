6f9b48b008eb8bfb3e69c2489dbb38221fa03031
Aisha <aisha.aysha216@gmail.com>
2024-04-20 20:38:35 +0500

    update changes

--- Q18. Seeing The World/main.ts
+++ Q18. Seeing The World/main.ts
@@ -1,8 +1,6 @@
 // Assignment # 18. Seeing the World: Think of at least five places in the world you’d like to visit.
 // • Store the locations in a array. Make sure the array is not in alphabetical order.
 
-import { log } from "console";
-
 // • Print your array in its original order.
 
 // • Print your array in alphabetical order without modifying the actual list.
